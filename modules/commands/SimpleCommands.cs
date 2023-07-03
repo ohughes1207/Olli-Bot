9f5be91e87695eaab1372829c4943e361df7899c
Olliver Hughes <ohughes1207@gmail.com>
2023-07-03 13:34:19 +0100

    Builds embed

--- modules/commands/SimpleCommands.cs
+++ modules/commands/SimpleCommands.cs
@@ -9,7 +9,20 @@ namespace self_bot.modules.commands
         [SlashCommand("avatar", "Get the avatar of the specified user")]
         public static async Task Avatar(InteractionContext ctx, [Option("user", "Specified user")] DiscordUser user)
         {
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(user.AvatarUrl));
+
+            var member = await ctx.Guild.GetMemberAsync(user.Id);
+            var nickname = member.Nickname ?? member.DisplayName ?? user.Username;
+            
+            //await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(user.AvatarUrl));
+            var embed = new DiscordEmbedBuilder();
+            embed.WithTitle($"{nickname}'s avatar");
+            //embed.WithColor(DiscordColor.Orange);
+            embed.WithColor(new DiscordColor(252, 177, 3));
+            embed.WithUrl(user.AvatarUrl);
+            embed.WithImageUrl(user.AvatarUrl);
+
+            await ctx.CreateResponseAsync(embed.Build());
+            //Console.Write(user);
         }
     }
 }
