90d10b05f5cefbd08b64f095752b0570fdbe88cc
Olliver Hughes <ohughes1207@gmail.com>
2024-03-16 18:06:54 +0000

    recreate message test

--- modules/commands/Database.cs
+++ modules/commands/Database.cs
@@ -13,14 +13,36 @@ namespace self_bot.modules.commands
     internal class DatabaseCommands : ApplicationCommandModule
     {
         [SlashCommand("AddDB", "Add an entry into the database")]
-        public async Task AddMessage(InteractionContext ctx, [Option("user", "Specified user")] string MessageID)
+        public async Task AddMessage(InteractionContext ctx, [Option("message", "Message ID")] string MessageID)
         {
             var channel = ctx.Channel;
             
             ulong messageUlong = Convert.ToUInt64(MessageID);
             var message = await channel.GetMessageAsync(messageUlong);
+
+            //Console.WriteLine(message.Attachments.);
             
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(message.Content));
+            //await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(message.Content));
+
+            var responseBuilder = new DiscordInteractionResponseBuilder();
+
+            string responseContent = message.Content;
+
+            // Check if the message has attachments
+            if (message.Attachments.Count > 0)
+            {
+                // For each attachment, add a new embed to the response
+                foreach (var attachment in message.Attachments)
+                {
+                    responseContent += $"{Environment.NewLine} {attachment.Url}";
+                }
+            }
+
+            responseBuilder.WithContent(responseContent);
+
+            // Send the response with the original message content and any attachments as embeds
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, responseBuilder);
+
         }
     }
 }
