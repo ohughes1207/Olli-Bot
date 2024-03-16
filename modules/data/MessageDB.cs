5b8047a736ff7176a55a0d7631dac3b80232c532
Olliver Hughes <ohughes1207@gmail.com>
2024-03-16 05:10:47 +0000

    Added Title property to Message class

--- modules/data/MessageDB.cs
+++ modules/data/MessageDB.cs
@@ -18,6 +18,7 @@ namespace self_bot.modules.data
         public ulong ID { get; set; }
         public ulong DiscordMessageID { get; set; }
         public ulong ServerID { get; set; }
+        public string? Title {get; set; }
 
         public required string Content { get; set; }
 
