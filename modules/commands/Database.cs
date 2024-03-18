a023d6d94d80aa30a54bee35280e73f81be7bd6c
Olliver Hughes <ohughes1207@gmail.com>
2024-03-18 07:22:04 +0000

    nullable title and try catch on add entry command

--- modules/commands/Database.cs
+++ modules/commands/Database.cs
@@ -13,49 +13,56 @@ namespace self_bot.modules.commands
         [SlashCommand("AddDB", "Add an entry into the database")]
         public async Task AddMessage(InteractionContext ctx,
         [Option("message", "Message ID")] string MessageID,
-        [Option("Title", "Title")] string? Title,
+        [Option("Title", "Title")] string? Title = null,
         [Choice("Meme", "Meme")] [Choice("Quote", "Quote")] [Choice("Other", "Other")] [Option("Type", "Type")] string? MessageType = null)
         {
-            var channel = ctx.Channel;
-
-            ulong messageUlong = Convert.ToUInt64(MessageID);
-            var message = await channel.GetMessageAsync(messageUlong);
-
-            if (MessageType == null)
+            try
             {
-                MessageType = GetMessageType(message, MessageType);
-            }
+                var channel = ctx.Channel;
 
-            var attList = new List<string>();
-            if (message.Attachments.Count > 0)
-            {
+                ulong messageUlong = Convert.ToUInt64(MessageID);
+                var message = await channel.GetMessageAsync(messageUlong);
 
-                foreach (var attachment in message.Attachments)
+                if (MessageType == null)
                 {
-                    attList.Add(attachment.Url);
+                    MessageType = GetMessageType(message, MessageType);
+                }
+
+                var attList = new List<string>();
+                if (message.Attachments.Count > 0)
+                {
+                    foreach (var attachment in message.Attachments)
+                    {
+                        attList.Add(attachment.Url);
+                    }
+                }
+
+                using (var db = new MessageDB())
+                {
+                    var newMessage = new Message
+                    {
+                        DiscordMessageID = message.Id,
+                        ServerID = ctx.Guild.Id,
+                        Title = Title,
+                        Content = message.Content,
+                        AttachmentUrls = attList,
+                        Author = ctx.User.Username,
+                        AuthorID = ctx.User.Id,
+                        MessageOriginID = message.Author.Id,
+                        MessageType = MessageType,
+                        DateTimeAdded = DateTime.UtcNow
+                    };
+
+                    await db.Messages.AddAsync(newMessage);
+                    await db.SaveChangesAsync();
+                    await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Entry added to the database").AsEphemeral());
                 }
             }
-
-            using (var db = new MessageDB())
+            catch (Exception ex)
             {
-                var newMessage = new Message
-                {
-                    DiscordMessageID = message.Id,
-                    ServerID = ctx.Guild.Id,
-                    Title = Title,
-                    Content = message.Content,
-                    AttachmentUrls = attList,
-                    Author = ctx.User.Username,
-                    AuthorID = ctx.User.Id,
-                    MessageOriginID = message.Author.Id,
-                    MessageType = MessageType,
-                    DateTimeAdded = DateTime.UtcNow
-                };
-
-                await db.Messages.AddAsync(newMessage);
-                await db.SaveChangesAsync();
-                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Entry added to the database").AsEphemeral());
+                Console.WriteLine(ex);
             }
+
         }
 
         private string GetMessageType(DiscordMessage message, string? messageType)
