dca01ca0ccd2c2789c449e95d754582ec83ecb2e
Olliver Hughes <ohughes1207@gmail.com>
2024-04-30 15:32:26 +0100

    removed old commented code and corrected bucket type to guild

--- modules/commands/Emotes.cs
+++ modules/commands/Emotes.cs
@@ -15,12 +15,13 @@ namespace self_bot.modules.commands
     internal class Emotes : ApplicationCommandModule
     {
         [SlashCommand("Emoterank", "Emote rankings")]
-        [SlashCooldown(1, 600, SlashCooldownBucketType.Channel)]
+        [SlashCooldown(1, 600, SlashCooldownBucketType.Guild)]
         public async Task RankEmotes(InteractionContext ctx)
         {
             try
             {
                 await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Bot is working on counting emotes").AsEphemeral());
+                
                 //Dictionary of emotes and an integer indicating number of uses 
                 var emoteCounts = new Dictionary<DiscordEmoji, int>();
 
@@ -35,18 +36,11 @@ namespace self_bot.modules.commands
                 {
                     Console.WriteLine(ch.Value.Name);
 
-                    //IReadOnlyList<DiscordMessage> messages = null;
-
                     ulong? lastMessageId = null;
                     while (true)
                     {
-                        /*if (lastMessageId==null)
-                        {
-                            break;
-                        }*/
                         IReadOnlyList<DiscordMessage> messages = lastMessageId == null ? await ch.Value.GetMessagesAsync(100) : await ch.Value.GetMessagesBeforeAsync(lastMessageId.Value, 100);
-                        //Console.WriteLine(lastMessageId);
-                        //Console.WriteLine(lastMessageId.Value);
+
                         if (messages.Count == 0)
                         {
                             break;
@@ -68,28 +62,18 @@ namespace self_bot.modules.commands
                         }
                     }
                 }
-                // Convert DiscordEmoji to string and calculate the maximum length for alignment
-                // int maxKeyLength = emoteCounts.Keys.Select(emoji => emoji.ToString().Length).Max();
 
                 // Create a formatted rank string with aligned data
                 var rankString = string.Join("\n", emoteCounts.OrderByDescending(kv => kv.Value).Select(kv => $"{kv.Key}  -  {kv.Value}"));
 
-                // Prepare the header and the full message string with proper markdown for code block
                 var header = "Emote Usage Ranking:";
                 var messageString = $"{header}\n{rankString}";
 
+
                 Console.WriteLine(messageString.Length);
 
-                // await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent(messageString));
-                // await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent(messageString));
-                await ctx.Channel.SendMessageAsync(messageString);
-                //await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(messageString));
                 // Send the formatted string as a single message to the Discord channel
-                //await ctx.Channel.SendMessageAsync(messageString);
-            }
-            catch (SlashExecutionChecksFailedException ex)
-            {
-                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(ex.Message).AsEphemeral());
+                await ctx.Channel.SendMessageAsync(messageString);
             }
             catch (Exception e)
             {
@@ -98,14 +82,3 @@ namespace self_bot.modules.commands
         }
     }
 }
-
-/*
-            foreach (var e in emoteList)
-            {
-                await ctx.Channel.SendMessageAsync(e.Value);
-                await Task.Delay(400);
-            }
-        }
-    }
-}
-*/
