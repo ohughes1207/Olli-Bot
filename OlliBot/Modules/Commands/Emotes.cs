2795628a6b1b287c9d569d3d2de24d0904f8459f
Olliver Hughes <ohughes1207@gmail.com>
2024-05-12 15:34:02 +0100

    improved logic

--- OlliBot/Modules/Commands/Emotes.cs
+++ OlliBot/Modules/Commands/Emotes.cs
@@ -19,38 +19,38 @@ namespace OlliBot.Modules
                 var emoteCounts = new Dictionary<DiscordEmoji, int>();
 
                 //only emotes that are available
-                var emoteList = ctx.Guild.Emojis.Where(e => e.Value.IsAvailable==true);
+                var emoteList = ctx.Guild.Emojis.Where(e => e.Value.IsAvailable);
 
                 //only text channels
-                var channelList = ctx.Guild.Channels.Where(c => c.Value.IsCategory==false && c.Value.Type==0);
+                var channelList = ctx.Guild.Channels.Where(c => !c.Value.IsCategory && c.Value.Type==0);
 
 
-                foreach (var ch in channelList)
+                foreach (var ch in channelList.Select(c => c.Value))
                 {
-                    Console.WriteLine(ch.Value.Name);
+                    Console.WriteLine(ch.Name);
 
                     ulong? lastMessageId = null;
                     while (true)
                     {
-                        IReadOnlyList<DiscordMessage> messages = lastMessageId == null ? await ch.Value.GetMessagesAsync(100) : await ch.Value.GetMessagesBeforeAsync(lastMessageId.Value, 100);
+                        IReadOnlyList<DiscordMessage> messages = lastMessageId == null ? await ch.GetMessagesAsync(100) : await ch.GetMessagesBeforeAsync(lastMessageId.Value, 100);
 
                         if (messages.Count == 0)
                         {
                             break;
                         }
                         lastMessageId = messages.Last().Id;
-                        foreach (var e in emoteList)
+                        foreach (var e in emoteList.Select(e => e.Value))
                         {
-                            var filteredMessages = from m in messages where (m.Content.Contains(e.Value) || m.Reactions.Any(reaction => reaction.Emoji.Equals(e.Value))) && m.Author.Id!=1118358168708329543 select m;
+                            var filteredMessages = from m in messages where (m.Content.Contains(e) || m.Reactions.Any(reaction => reaction.Emoji.Equals(e))) && m.Author.Id!=1118358168708329543 select m;
                             int count = filteredMessages.Count();
 
-                            if (emoteCounts.ContainsKey(e.Value))
+                            if (emoteCounts.ContainsKey(e))
                             {
-                                emoteCounts[e.Value]+=count;
+                                emoteCounts[e]+=count;
                             }
                             else
                             {
-                                emoteCounts[e.Value]=count;
+                                emoteCounts[e]=count;
                             }
                         }
                     }
