f91ebf0ddc251762a114469d6deaedc96346186b
Olliver Hughes <ohughes1207@gmail.com>
2024-05-20 05:16:02 +0100

    Migration progress

--- OlliBot/Bot.cs
+++ OlliBot/Bot.cs
@@ -1,121 +1,37 @@
-using System.Text.Json;
 using DSharpPlus;
-using DSharpPlus.EventArgs;
-using DSharpPlus.SlashCommands;
-using System.Text.Json.Serialization;
-using DSharpPlus.SlashCommands.EventArgs;
-using DSharpPlus.SlashCommands.Attributes;
 using DSharpPlus.Entities;
 using OlliBot.Modules;
-using OlliBot.Data;
 
+namespace OlliBot;
 
-namespace OlliBot
+public class Bot : BackgroundService
 {
-    public class Bot
+    private readonly ILogger<Bot> _logger;
+
+    private readonly DiscordClient _discordClient;
+
+    public Bot(ILogger<Bot> logger, DiscordClient discordClient)
     {
-        internal Config Config {get; private set;}
-        internal DiscordClient Client { get; private set; }
-        internal SlashCommandsExtension Slash { get; private set; }
+        _logger = logger;
+        _discordClient = discordClient;
 
-        public Bot()
-        {
-            Config = InitializeConfig();
+    }
+    public override async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Started");
+        _discordClient.Ready += BotInitialization.InitializationTasks;
+        await _discordClient.ConnectAsync(new DiscordActivity("you", ActivityType.ListeningTo));
+    }
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        //_discordClient.Dispose();
+        await _discordClient.DisconnectAsync();
+        //_discordClient.Dispose();
+    }
 
-            var clientConfig = new DiscordConfiguration
-            {
-                Token = Config.Token,
-                TokenType = TokenType.Bot,
-                AutoReconnect = true,
-                Intents = DiscordIntents.All
-                };
-
-            Client = new DiscordClient(clientConfig);
-            Slash = Client.UseSlashCommands();
-        }
-        
-        public async Task RunAsync()
-        {
-            try
-            {
-                Console.Write(Config.OwnerID);
-
-                Client.Ready += BotInitialization.InitializationTasks;
-                
-                
-                /*var slashConfig = new SlashCommandsConfiguration
-                {
-                    
-                };
-
-                Slash = Client.UseSlashCommands(slashConfig);*/
-
-                Slash.SlashCommandErrored += OnSlashError;
-                SlashRegistry.RegisterCommands(Slash);
-
-                await Client.ConnectAsync(new DiscordActivity("you", ActivityType.ListeningTo));
-                await Task.Delay(-1);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error running bot..\n------- EXCEPTION -------\n {ex.Message}\n------- EXCEPTION -------");
-            }
-        }
-        private async Task OnSlashError(SlashCommandsExtension sender, SlashCommandErrorEventArgs e)
-        {
-            // Check if the error is due to a failed check i.e on cooldown
-            if (e.Exception is SlashExecutionChecksFailedException checksFailedException)
-            {
-                foreach (var check in checksFailedException.FailedChecks)
-                {
-                    // Slash command is on cooldown
-                    if (check is SlashCooldownAttribute cooldown)
-                    {
-                        await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder()
-                        .WithContent($"This command is on cooldown. Please wait {Math.Round(cooldown.GetRemainingCooldown(e.Context).TotalSeconds, 1)} seconds.")
-                        .AsEphemeral(true));
-                    }
-                    // Command failed because some other check failed (issue with permissions maybe?)
-                    else
-                    {
-                        await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder()
-                        .WithContent("You do not have permission to use this command.")
-                        .AsEphemeral(true));
-                    }
-                }
-            }
-            else
-            {
-                // Case for typical exceptions
-                Console.WriteLine(e.Exception);
-            }
-        }
-        /*
-        private async Task OnSlashError(SlashCommandsExtension sender, SlashCommandErrorEventArgs e)
-        {
-            if (e.Exception is SlashExecutionChecksFailedException slashex)
-            {
-
-            }
-            //Console.WriteLine(sender);
-            Console.WriteLine(e.Exception);
-        }
-        private Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
-        {
-            return Task.CompletedTask;
-        }
-        */
-        private Config InitializeConfig()
-        {
-            var configFile = File.ReadAllText(@"config.json");
-            Config = JsonSerializer.Deserialize<Config>(configFile) ?? throw new InvalidOperationException("Config cannot be null");
-            return Config;
-        }
-
-        public async Task SaveConfig()
-        {
-            var config = JsonSerializer.Serialize(Config, new JsonSerializerOptions { WriteIndented = true});
-            await File.WriteAllTextAsync($"config.json", config);
-        }
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine("Executed");
+        return Task.CompletedTask;
     }
 }
--- OlliBot/Program.cs
+++ OlliBot/Program.cs
@@ -1,7 +1,41 @@
-using OlliBot;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+namespace OlliBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
 
-var host = builder.Build();
-host.Run();
+            builder.Services.AddHostedService<Bot>();
+
+            builder.Services.AddSingleton<DiscordClient>((serviceProvider) =>
+            {
+                var config = builder.Configuration;
+
+                var discordClient = new DiscordClient(new DiscordConfiguration
+                {
+                    Token = config["DiscordBotToken"],
+                    TokenType = TokenType.Bot,
+                    AutoReconnect = true,
+                    Intents = DiscordIntents.All
+                });
+
+                var slash = discordClient.UseSlashCommands(new SlashCommandsConfiguration
+                {
+                    Services = serviceProvider
+                });
+
+                SlashRegistry.RegisterCommands(slash);
+
+                return discordClient;
+            });
+
+            var host = builder.Build();
+            //Console.Write(builder.Services.GetType());
+            host.Run();
+        }
+    }
+}
