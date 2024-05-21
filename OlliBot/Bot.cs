7a6cb69cee5cf34de66c9eb22948709231e6f20d
Olliver Hughes <ohughes1207@gmail.com>
2024-05-21 10:16:32 +0100

    Log info on start and shutdown

--- OlliBot/Bot.cs
+++ OlliBot/Bot.cs
@@ -1,9 +1,11 @@
 using DSharpPlus;
+using DSharpPlus.AsyncEvents;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.Attributes;
 using DSharpPlus.SlashCommands.EventArgs;
 using OlliBot.Modules;
+using OlliBot.Utilities;
 
 namespace OlliBot;
 
@@ -14,6 +16,7 @@ public class Bot : BackgroundService
     private readonly DiscordClient _discordClient;
 
     private readonly SlashCommandsExtension _slash;
+
     public Bot(ILogger<Bot> logger, DiscordClient discordClient, SlashCommandsExtension slash)
     {
         _logger = logger;
@@ -23,54 +26,43 @@ public class Bot : BackgroundService
     
     public override async Task StartAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("Started");
-        //_discordClient.Ready += BotInitialization.InitializationTasks;
-        //_slash.SlashCommandErrored += OnSlashError;
-        await _discordClient.ConnectAsync(new DiscordActivity("you :3", ActivityType.ListeningTo));
+        _logger.LogInformation("OlliBot starting...");
+        _discordClient.Ready += BotInitialization.InitializationTasks;
+        _slash.SlashCommandErrored += ExceptionHandler.OnSlashError;
+
+        await _discordClient.ConnectAsync(new DiscordActivity("you :3", ActivityType.Watching));
     }
+
+
     public override async Task StopAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("STOPPING");
-        await _discordClient.DisconnectAsync();
-        _discordClient.Dispose();
-        _logger.LogInformation("Bot client disposed.");
-        //_slash.Dispose();
+        try
+        {
+            _discordClient.Ready -= BotInitialization.InitializationTasks;
+            _slash.SlashCommandErrored -= ExceptionHandler.OnSlashError;
+
+
+            _logger.LogInformation("OlliBot disconnecting...");
+            await _discordClient.DisconnectAsync();
+            _logger.LogInformation("Ollibot disconnected...");
+
+
+            _logger.LogInformation("Disposing client...");
+            _slash.Dispose();
+            _discordClient.Dispose();
+            _logger.LogInformation("Bot client disposed...");
+
+
+            _logger.LogInformation("OlliBot shutting down...");
+        }
+        catch (Exception ex) 
+        {
+            _logger.LogError(ex.Message);
+        }
     }
 
+
+    //Only returns Task.CompletedTask to satisfy implementation requirement for BackgroundService
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        //Console.WriteLine("Executed");
-        return Task.CompletedTask;
-    }
-
-    //Move this logic to a new class
-    private async Task OnSlashError(SlashCommandsExtension sender, SlashCommandErrorEventArgs e)
-    {
-        // Check if the error is due to a failed check i.e on cooldown
-        if (e.Exception is SlashExecutionChecksFailedException checksFailedException)
-        {
-            foreach (var check in checksFailedException.FailedChecks)
-            {
-                // Slash command is on cooldown
-                if (check is SlashCooldownAttribute cooldown)
-                {
-                    await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder()
-                    .WithContent($"This command is on cooldown. Please wait {Math.Round(cooldown.GetRemainingCooldown(e.Context).TotalSeconds, 1)} seconds.")
-                    .AsEphemeral(true));
-                }
-                // Command failed because some other check failed (issue with permissions maybe?)
-                else
-                {
-                    await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder()
-                    .WithContent("You do not have permission to use this command.")
-                    .AsEphemeral(true));
-                }
-            }
-        }
-        else
-        {
-            // Case for typical exceptions
-            Console.WriteLine(e.Exception);
-        }
-    }
+        => Task.CompletedTask;
 }
