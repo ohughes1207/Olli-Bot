7c029572c0edad200590b01b134c0bdc28677d10
Olliver Hughes <ohughes1207@gmail.com>
2024-05-24 23:00:53 +0100

    Set bot Id from client

--- OlliBot/Modules/BotInitialization.cs
+++ OlliBot/Modules/BotInitialization.cs
@@ -5,8 +5,21 @@ namespace OlliBot.Modules
 {
     public class BotInitialization
     {
-        public static async Task InitializationTasks(DiscordClient Client, ReadyEventArgs args)
+        private readonly IConfiguration _configuration;
+        private readonly Serilog.ILogger _logger;
+        private readonly DiscordClient _discordClient;
+        public BotInitialization(IConfiguration configuration, Serilog.ILogger logger, DiscordClient discordClient)
         {
+            _configuration = configuration;
+            _logger = logger;
+            _discordClient = discordClient;
+        }
+
+        public async Task InitializationTasks(DiscordClient Client, ReadyEventArgs args)
+        {
+            _logger.Information("Initialization tasks running...");
+            _configuration["BotID"] = _discordClient.CurrentUser.Id.ToString();
+            _logger.Information($"Bot ID: {_configuration["BotID"]}");
             Client.MessageCreated += EventHandler.OnMessage;
             await Task.CompletedTask;
         }
