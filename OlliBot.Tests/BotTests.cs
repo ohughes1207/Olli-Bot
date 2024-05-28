894a90afff3a3cad2bdb21dda0d00048212ddf04
Olliver Hughes <ohughes1207@gmail.com>
2024-05-28 01:22:43 +0100

    basic test setup

---
+++ OlliBot.Tests/BotTests.cs
@@ -0,0 +1,28 @@
+
+
+
+namespace OlliBot.Tests
+{
+    public class BotTests : IDisposable
+    {
+        public BotTests()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
