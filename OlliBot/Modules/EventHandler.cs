03bb21924d9b9c6e46e8f601ae3f87ecc7957598
Olliver Hughes <ohughes1207@gmail.com>
2024-06-02 15:08:59 +0100

    OnSlashExecute event

--- OlliBot/Modules/EventHandler.cs
+++ OlliBot/Modules/EventHandler.cs
@@ -1,75 +1,88 @@
-using DSharpPlus;
-using DSharpPlus.Entities;
-using DSharpPlus.EventArgs;
-using DSharpPlus.SlashCommands;
-using DSharpPlus.SlashCommands.EventArgs;
+using Discord.WebSocket;
+using Discord;
 using System.Text;
+using Discord.Interactions;
 
 namespace OlliBot.Modules
 {
     public class EventHandler
     {
         private readonly IConfiguration _configuration;
+        private readonly DiscordSocketClient _client;
+        private readonly ILogger<Bot> _logger;
 
-        public EventHandler(IConfiguration configuration)
+        public EventHandler(IConfiguration configuration, DiscordSocketClient client, ILogger<Bot> logger)
         {
             _configuration = configuration;
+            _client = client;
+            _logger = logger;
         }
 
-        public async Task OnMessage(DiscordClient Client, MessageCreateEventArgs args)
+        public async Task OnMessage(SocketMessage message)
         {
-            
+            SocketGuildChannel channel = (SocketGuildChannel)message.Channel;
+            var guild = channel.Guild;
 
-            if (args.Message.Content.Length > 150 && args.Message.Author.Id!=Client.CurrentUser.Id)
+            if (message.Content.Length > 150 && message.Author.Id != _client.CurrentUser.Id)
             {
-                await Client.SendMessageAsync(args.Channel, new DiscordMessageBuilder().WithReply(args.Message.Id).WithContent("i ain't reading all that"));
-                await Client.SendMessageAsync(args.Channel, new DiscordMessageBuilder().WithContent("i'm happy for u tho"));
-                await Client.SendMessageAsync(args.Channel, new DiscordMessageBuilder().WithContent("or sorry that happened"));
+                await message.Channel.SendMessageAsync("i ain't reading all that", messageReference: new MessageReference(message.Id));
+                await message.Channel.SendMessageAsync("i'm happy for u tho");
+                await message.Channel.SendMessageAsync("or sorry that happened");
                 return;
             }
 
-            if (args.Message.Author.Id==164740251934392321 && args.Guild.Id.ToString() == _configuration["MainServer"] && new Random().Next(1, 101)<=15)
+            if (message.Author.Id== 164740251934392321 && guild.Id.ToString() == _configuration["MainServer"] && new Random().Next(1, 101)<=15)
             {
-                await Client.SendMessageAsync(args.Channel, new DiscordMessageBuilder().WithReply(args.Message.Id).WithContent("James here"));
+                await message.Channel.SendMessageAsync("James Here", messageReference: new MessageReference(message.Id));
             }
 
 
             // Add more logic to this event
         }
-
-        public Task OnSlashInvoke(SlashCommandsExtension Slash, SlashCommandInvokedEventArgs args)
+        public Task OnSlashExecute(SlashCommandInfo slashInfo, IInteractionContext ctx, IResult result)
         {
-            StringBuilder logMessage = new StringBuilder();
-            logMessage.Append($"Command invoked: {args.Context.CommandName} ");
-
-            if (args.Context.Interaction.Data.Options != null)
+            if (result.IsSuccess)
             {
-                logMessage.Append("(");
-                foreach (var option in args.Context.Interaction.Data.Options.Where(option => option != null))
-                {
-                    logMessage.Append($"{option.Name}:{option.Value}, ");
-                }
-                if (logMessage[logMessage.Length - 2] == ',') // Removing the trailing comma and space
-                {
-                    logMessage.Length -= 2;
-                }
-                logMessage.Append(") ");
+                _logger.LogInformation("Command executed successfully");
             }
-            logMessage.Append($"by {args.Context.User.Username}, {args.Context.User.Id}");
-
-            if (!string.IsNullOrEmpty(args.Context.Member.Nickname))
+            else if (!result.IsSuccess)
             {
-                logMessage.Append($" ({args.Context.Member.Nickname})");
+                if (result.Error == InteractionCommandError.UnmetPrecondition)
+                {
+                    _logger.LogWarning($"{result.ErrorReason}");
+                }
             }
-
-            Slash.Client.Logger.LogInformation($"{logMessage}");
-            return Task.CompletedTask;
-        }
-
-        public Task OnSlashExecute(SlashCommandsExtension Slash, SlashCommandExecutedEventArgs args)
-        {
-            Slash.Client.Logger.LogInformation("Command executed successfully");
             return Task.CompletedTask;
         }
     }
 }
+
+/*
+public Task OnSlashInvoke(SlashCommandsExtension Slash, SlashCommandInvokedEventArgs args)
+{
+    StringBuilder logMessage = new StringBuilder();
+    logMessage.Append($"Command invoked: {args.Context.CommandName} ");
+
+    if (args.Context.Interaction.Data.Options != null)
+    {
+        logMessage.Append("(");
+        foreach (var option in args.Context.Interaction.Data.Options.Where(option => option != null))
+        {
+            logMessage.Append($"{option.Name}:{option.Value}, ");
+        }
+        if (logMessage[logMessage.Length - 2] == ',') // Removing the trailing comma and space
+        {
+            logMessage.Length -= 2;
+        }
+        logMessage.Append(") ");
+    }
+    logMessage.Append($"by {args.Context.User.Username}, {args.Context.User.Id}");
+
+    if (!string.IsNullOrEmpty(args.Context.Member.Nickname))
+    {
+        logMessage.Append($" ({args.Context.Member.Nickname})");
+    }
+
+    Slash.Client.Logger.LogInformation($"{logMessage}");
+    return Task.CompletedTask;
+}*/
