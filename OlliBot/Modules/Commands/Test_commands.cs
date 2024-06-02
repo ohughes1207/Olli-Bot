396a2299c278e6ca03bc7204821e24b7c756f315
Olliver Hughes <ohughes1207@gmail.com>
2024-06-02 19:01:47 +0100

    fixed test commands

--- OlliBot/Modules/Commands/Test_commands.cs
+++ OlliBot/Modules/Commands/Test_commands.cs
@@ -1,34 +1,28 @@
-using DSharpPlus;
-using DSharpPlus.Entities;
-using DSharpPlus.SlashCommands;
-using DSharpPlus.SlashCommands.Attributes;
+using Discord.Interactions;
 
 namespace OlliBot.Modules
 {
-    public class TestCommands : ApplicationCommandModule
+    public class TestCommands : InteractionModuleBase<SocketInteractionContext>
     {
         [SlashCommand("test1", "A slash command test")]
-        [SlashCooldown(1, 10, SlashCooldownBucketType.User)]
-        public async Task TestCommand(InteractionContext ctx)
+        public async Task TestCommand()
         {
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("All systems functional\nTest success").AsEphemeral());
+            await Context.Interaction.RespondAsync("All systems functional\nTest success", ephemeral: true);
         }
 
         [SlashCommand("test3", "3rd slash command test")]
-        public async Task TestCommand2(InteractionContext ctx)
+        public async Task TestCommand2()
         {
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("womp womp").AsEphemeral());
+            await Context.Interaction.RespondAsync("womp womp", ephemeral: true);
         }
     }
-
-    //Created a new class just to see how that interacts with registering new commannds
-    public class TestCommands2 : ApplicationCommandModule
+    public class TestCommands2 : InteractionModuleBase<SocketInteractionContext>
     {
 
         [SlashCommand("test2", "Another slash command test")]
-        public async Task TestCommand(InteractionContext ctx)
+        public async Task TestCommand()
         {
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("wow").AsEphemeral());
+            await Context.Interaction.RespondAsync("wow", ephemeral: true);
         }
     }
 }
