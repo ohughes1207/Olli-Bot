2065d7b7e63a37f600f00c3572d4969ffbad8426
Olliver Hughes <ohughes1207@gmail.com>
2024-07-30 04:14:21 +0100

    Helpers class and URL check method

---
+++ OlliBot/Utilities/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OlliBot.Utilities
+{
+    public class Helpers
+    {
+        public static bool HasURL(string? input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+
+            var regex = new Regex(@"https?://[^\s/$.?#].[^\s]*");
+            return regex.IsMatch(input);
+        }
+    }
+}
